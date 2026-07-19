0c9732da4f49bd10bb2160e023b2ade1940dc550
Artur2 <arturnurullin@outlook.com>
2026-07-19 17:47:16 +0500

    Few todos

--- src/graph.rs
+++ src/graph.rs
@@ -1,12 +1,15 @@
-use std::cell::RefCell;
-
+use crate::nodes::edge::Edge;
 use crate::nodes::vertex::Vertex;
+use std::cell::RefCell;
+use std::rc::Rc;
 
 #[derive(Default)]
 #[allow(dead_code)]
 pub struct Graph {
     pub name: String,
     pub vertices: Vec<RefCell<Vertex>>,
+    pub vertices_references: Vec<Rc<RefCell<Vertex>>>,
+    pub edges_references: Vec<Rc<RefCell<Edge>>>,
 }
 
 #[allow(dead_code)]
@@ -15,6 +18,20 @@ impl Graph {
         Graph {
             name,
             vertices: vec![],
+            vertices_references: vec![],
+            edges_references: vec![],
         }
+    }
+
+    pub fn add_vertex(&mut self, name: String) {
+        todo!()
+    }
+
+    pub fn add_vertex_to_edge(&mut self, name: String, weight: u32) {
+        todo!()
+    }
+
+    pub fn add_edge_to_vertex(&mut self, name: String, weight: u32) {
+        todo!()
     }
 }
--- src/nodes/edge.rs
+++ src/nodes/edge.rs
@@ -5,6 +5,7 @@ use std::cell::RefCell;
 #[derive(Default)]
 #[allow(dead_code)]
 pub struct Edge {
+    pub name: String,
     pub weight: u32,
     pub edges: Vec<RefCell<Vertex>>,
 }
