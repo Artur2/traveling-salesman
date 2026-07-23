9e6dae799baa372fc6f98e57b339a4367abb3f54
Artur2 <arturnurullin@outlook.com>
2026-07-23 10:56:03 +0500

    Refactoring

--- src/path_resolver.rs
+++ src/path_resolver.rs
@@ -1,9 +1,10 @@
 use crate::graph::Graph;
 use crate::processing_algorithm::ProcessingAlgorithm;
+use crate::types::MutableVertexReferences;
 
 pub struct PathResolver<'a> {
-    pub graph: Graph<'a>,
-    pub processing_algorithm: ProcessingAlgorithm,
+    graph: Graph<'a>,
+    processing_algorithm: ProcessingAlgorithm,
 }
 
 impl<'a> PathResolver<'a> {
@@ -12,5 +13,39 @@ impl<'a> PathResolver<'a> {
             graph: Graph::new(graph_name),
             processing_algorithm: ProcessingAlgorithm::default(),
         }
+    }
+
+    pub fn add_vertex(&mut self, vertex_name: &'a str) {
+        self.graph.add_vertex(vertex_name);
+    }
+
+    pub fn connect_vertices(
+        &mut self,
+        source_vector_name: &str,
+        destination_vector_name: &str,
+        edge_identifier: &'a str,
+        weight: u32,
+    ) {
+        self.graph.connect_vertices(
+            source_vector_name,
+            destination_vector_name,
+            edge_identifier,
+            weight,
+        );
+    }
+
+    pub fn resolve_optimal_path(
+        &self,
+        source: &str,
+        destination: &str,
+    ) -> MutableVertexReferences<'a> {
+        let random_paths = self.processing_algorithm.generate_random_routes(
+            &self.graph.vertex_references,
+            source,
+            destination,
+            100,
+        );
+
+        todo!("Implement optimal path")
     }
 }
